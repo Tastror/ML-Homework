3693226232e3ecc78385ee7a09777c4a64d71efc
Tastror <yccozy2001@163.com>
2023-03-23 18:26:32 +0800

    update dataloader

--- train.py
+++ train.py
@@ -4,17 +4,26 @@ import torch.nn as nn
 import torch.optim as optim
 from datetime import datetime
 from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 from tqdm import tqdm
 from model.model_1 import Net
 
-# 数据预处理
-train_dataset = MNIST(root='data/', train=True, transform=ToTensor(), download=True)
-test_dataset = MNIST(root='data/', train=False, transform=ToTensor(), download=True)
+# 数据读入 I（如果你使用 MNIST）
+# from torchvision.datasets import MNIST
+# train_dataset = MNIST(root='data/', train=True, transform=ToTensor(), download=True)
+# test_dataset = MNIST(root='data/', train=False, transform=ToTensor(), download=True)
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+# 数据读入 II
+from utils.dataloader import Dataset
+data_dir = 'dataset'
+data_name = 'NewDataset.mat'
+data_path = os.path.join(data_dir, data_name)
+train_dataset = Dataset(data_path, train=True, transform=ToTensor())
+test_dataset = Dataset(data_path, train=False, transform=ToTensor())
+
+# 数据转为 data
+train_data = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_data = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 # 保存位置处理
 save_dir = 'weight'
@@ -32,11 +41,12 @@ model = Net().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+
 class Accuracy:
     def __init__(self):
         self.correct = 0
         self.total = 0
-        
+
     def update(self, outputs, targets):
         _, predicted = torch.max(outputs.data, 1)
         self.total += targets.size(0)
@@ -45,6 +55,7 @@ class Accuracy:
     def compute(self):
         return 100.0 * self.correct / self.total
 
+# 定义准确率计算器
 accuracy = Accuracy()
 
 # 训练模型
@@ -52,21 +63,21 @@ num_epochs = 4
 for epoch in range(num_epochs):
     running_loss = 0.0
     running_acc = 0.0
-    for images, labels in tqdm(train_loader):
+    for images, labels in tqdm(train_data):
         images = images.to(device)
         labels = labels.to(device)
-        
+
         optimizer.zero_grad()
-        
+
         outputs = model(images)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        
+
         running_loss += loss.item()
         accuracy.update(outputs, labels)
 
-    print(f'Epoch {epoch+1}/{num_epochs}, Loss: {running_loss/len(train_loader):.4f}, Acc: {accuracy.compute():.4f}')
+    print(f'Epoch {epoch+1}/{num_epochs}, Loss: {running_loss/len(train_data):.4f}, Acc: {accuracy.compute():.4f}')
 
 torch.save(model.state_dict(), save_path)
 torch.save(model.state_dict(), last_save_path)
@@ -76,14 +87,14 @@ model.eval()
 with torch.no_grad():
     correct = 0
     total = 0
-    for images, labels in tqdm(test_loader):
+    for images, labels in tqdm(test_data):
         images = images.to(device)
         labels = labels.to(device)
-        
+
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
-        
+
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
-        
+
     print(f'Test Accuracy: {correct/total:.4f}')
---
+++ utils/dataloader.py
@@ -0,0 +1,56 @@
+import numpy as np
+import scipy.io as scio
+from torch.utils.data.dataset import Dataset
+
+
+class Dataset(Dataset):
+    def __init__(self, path, train, transform):
+        super(Dataset, self).__init__()
+
+        self.data = scio.loadmat(path)
+        self.data = self.data['train'] if train else self.data['test']
+        self.shape = np.shape(self.data)
+        self.data_shape = self.shape[:2]
+        self.input_shape = self.shape[2:]
+        self.length = self.data_shape[0] * self.data_shape[1]
+
+        print('length of dataset is', self.length, end=", ")
+        print('shape of dataset is', self.data_shape, end=", ")
+        print('shape of image is', self.input_shape)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+
+        if index >= self.length or index < -self.length:
+            raise IndexError(
+                f"Dataset index out of boundary: {index} in 0 ~ {self.length - 1}"
+            )
+        if index < 0:
+            index = self.length + index
+        label = index // self.data_shape[1]
+        index = index % self.data_shape[1]
+        print(label, index)
+        image = self.data[label][index]
+
+        # 添加通道维度，正则化为 [0, 1]，更换顺序（通道放在最前面）
+        image = np.expand_dims(image, axis=2)
+        image = np.transpose(
+            np.array(image, dtype=np.float32) / 255, (2, 0, 1)
+        )
+
+        return image, label
+
+
+# DataLoader 中 collate_fn 使用
+# def dataset_collate(batch):
+#     images = []
+#     Label = []
+#     for img, label in batch:
+#         images.append(img)
+#         Label.append(label)
+
+#     images = torch.from_numpy(np.array(images)).type(torch.FloatTensor)
+#     Label = torch.from_numpy(np.array(Label)).type(torch.LongTensor)
+#     return images, Label
